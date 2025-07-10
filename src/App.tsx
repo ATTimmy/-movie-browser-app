1eec663f1d21e7c49cebf1f05b5658d76e9fd4d8
Timmy <ivanjc92web@gmail.com>
2025-07-10 20:23:04 +0200

    test: test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-
 export default function App() {
   const [count, setCount] = useState(0);
 
@@ -12,9 +11,6 @@ export default function App() {
       <h1>Hello world!</h1>
       <p>Count: {count}</p>
       <button onClick={increment}>Increment</button>
-      <button onClick={() => setCount(count + 1)} disabled>
-        Increment 2
-      </button>
     </div>
   );
 }
