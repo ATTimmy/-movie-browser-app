c254d1cac8fd0fcd54758b9b7d94b838bd556b45
Timmy <ivanjc92web@gmail.com>
2025-07-11 13:44:38 +0200

    chore: fix Prettier formatting for CI compliance

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './styles/main.scss';
+import './Styles/main.scss';
 
 // Hydrate the app on the client using the server-rendered HTML in #root.
 // Wrap with BrowserRouter to enable client-side routing.
